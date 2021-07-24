8431d4b03cb75b3497dba944657d583f64a9d16e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-07-24 08:49:25 +0200

    Commenties

--- openWeatherLibrary/openWeatherLibrary.c
+++ openWeatherLibrary/openWeatherLibrary.c
@@ -9,13 +9,13 @@ typedef struct openWeather {
 
 } OpenWeather;
 
-// Definition of the 'getWeather' function...
+// Definition of the 'getWeather' function, which takes the wished city and the OpenWeatherMap api key in argument...
 void getWeather(char* city, char* apiKey){
 
     printf("Hello, World!\n");
 }
 
-//
+// Definition of the 'getWeather' function, which takes the wished city, the corresponding country code and the OpenWeatherMap api key in argument...
 void getWeather(char* city, char* countryCode, char* apiKey){
 
     printf("Hello, World!\n");
