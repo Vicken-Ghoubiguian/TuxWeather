783f3ada3c44ed0a416b4a77cd26e26495356193
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-07-25 09:21:40 +0200

    For localisation

--- openWeatherLibrary/openWeatherLibrary.c
+++ openWeatherLibrary/openWeatherLibrary.c
@@ -19,6 +19,9 @@ typedef struct openWeather {
     // Field for timezone (number of seconds of offset from UTC)...
     long int timezone;
 
+    // Field for the localisation's name...
+    char* name;
+
     // Field for HTTP/HTTPS result code...
     int cod;
 
